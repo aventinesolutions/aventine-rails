3d116725e0497e2e5de569a347d4e3fdaf5179bb
matthew <matthew.eichler@aventinesolutions.nl>
2012-10-31 11:25:50 +0100

    intialized Rails 3.2.8 project with Rubymine

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AventineRails::Application.config.secret_token = '8f2a4faab9fcb52e0dcf81fc291725143ce6195ce3d810153b65f3be76791617f7650347f91d70d082e18e029fb9bc036a7dab6e231126ad279a0076a26b61ae'
