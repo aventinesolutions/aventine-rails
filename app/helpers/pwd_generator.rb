ea1124ccd947cd9b06ef583600f351fd577b28ee
Matthew Eichler <matthew.eichler@aventinesolutions.nl>
2016-01-21 14:13:08 +0100

    :bear: use the more DRY `times`

--- app/helpers/pwd_generator.rb
+++ app/helpers/pwd_generator.rb
@@ -24,20 +24,20 @@ class PwdGenerator
 
   def generate
     passwords = []
-    (1..@no_passwords).each do
+    @no_passwords.times do
       words = []
-      (1..@no_words).each do |i|
+      @no_words.times do |i|
         rule = RULES[i%RULES.length]
         word = ''
         case rule
           when :uppercase
-            (1..@no_uppercase_alpha).each { word += "ABCDEFGHIJKLMNOPQRSTUVWXYZ"[RANDOM.rand(0..25), 1] }
+            @no_uppercase_alpha.times { word += "ABCDEFGHIJKLMNOPQRSTUVWXYZ"[RANDOM.rand(0..25), 1] }
           when :lowercase
-            (1..@no_lowercase_alpha).each { word += "abcdefghijklmnopqrstuvwxyz"[RANDOM.rand(0..25), 1] }
+            @no_lowercase_alpha.times { word += "abcdefghijklmnopqrstuvwxyz"[RANDOM.rand(0..25), 1] }
           else
             strokes = []
-            (1..@no_numerics).each { strokes << "0123456789"[RANDOM.rand(0..8), 1] }
-            (1..@no_symbols).each { strokes << '!@#$&*.:?+='[RANDOM.rand(0..10), 1] }
+            @no_numerics.times { strokes << "0123456789"[RANDOM.rand(0..8), 1] }
+            @no_symbols.times { strokes << '!@#$&*.:?+='[RANDOM.rand(0..10), 1] }
             word = strokes.shuffle.join('')
         end
         words << word
